b0ac3bc0c927a9dd315476aa7f540342fb567e27
louchushuai <louchushuai@126.com>
2019-06-24 11:30:03 +0800

    add param

--- src/main/java/com/jiyu/git/controller/CartController.java
+++ src/main/java/com/jiyu/git/controller/CartController.java
@@ -3,6 +3,7 @@ package com.jiyu.git.controller;
 public class CartController {
 	
 	public static void main(String[] args) {
-		System.out.println("hello lousanniu");
+		String name = "lousanniu";
+		System.out.println("hello "+name);
 	}
 }
