8dd989d887cefd6bded4a86633e565e6aeb2f8b5
lousanniu <louchushuai@126.com>
2019-06-25 13:06:03 +0800

    add a map

--- src/main/java/com/jiyu/git/controller/CartController.java
+++ src/main/java/com/jiyu/git/controller/CartController.java
@@ -1,8 +1,12 @@
 package com.jiyu.git.controller;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class CartController {
 	
 	public static void main(String[] args) {
+		Map<String, String> map = new ConcurrentHashMap<String, String>(10);
 		System.out.println("hello");
 	}
 }
